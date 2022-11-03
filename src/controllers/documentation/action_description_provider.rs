d8980b39c90ae1d95b755b353c64daab4d061ce9
amigin <amigin@gmail.com>
2022-11-03 17:28:56 +0200

    Now we can add summary and description

--- src/controllers/documentation/action_description_provider.rs
+++ src/controllers/documentation/action_description_provider.rs
@@ -2,6 +2,7 @@ use super::{in_parameters::HttpInputParameter, out_results::HttpResult};
 
 pub struct HttpActionDescription<'s> {
     pub controller_name: &'s str,
+    pub summary: &'s str,
     pub description: &'s str,
     pub input_params: Option<Vec<HttpInputParameter>>,
     pub results: Vec<HttpResult>,

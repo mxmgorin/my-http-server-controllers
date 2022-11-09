6deb5a9e15e9878518b8d6f28bbc3a012503042e
amigin <amigin@gmail.com>
2022-11-10 00:55:18 +0200

    Debugging

--- src/controllers/file_content_input_data.rs
+++ src/controllers/file_content_input_data.rs
@@ -1,4 +1,21 @@
+use std::str::FromStr;
+
+use my_http_server::HttpFailResult;
+
 pub struct FileContent {
     pub file_name: String,
     pub content: Vec<u8>,
+}
+
+impl FromStr for FileContent {
+    type Err = HttpFailResult;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let result = FileContent {
+            file_name: s.to_string(),
+            content: Vec::new(),
+        };
+
+        Ok(result)
+    }
 }

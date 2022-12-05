faa8de86b3ce4b21ec197782bf9daa908848fa95
amigin <amigin@gmail.com>
2022-12-05 09:59:37 +0200

    Added Fail result Swagger Description

--- src/controllers/auth_error_factory.rs
+++ src/controllers/auth_error_factory.rs
@@ -1,6 +1,9 @@
 use my_http_server::HttpFailResult;
 
+use super::documentation::out_results::HttpResult;
+
 pub trait AuthErrorFactory {
     fn get_not_authenticated(&self) -> HttpFailResult;
     fn get_not_authorized(&self, claim_name: String) -> HttpFailResult;
+    fn get_global_http_fail_result_types(&self) -> Option<Vec<HttpResult>>;
 }

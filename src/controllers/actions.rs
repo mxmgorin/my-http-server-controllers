b17d684c12ad819c8fe1689e7c865d997d67f66d
amigin <amigin@gmail.com>
2022-12-06 11:11:29 +0200

    Fixing Bug

--- src/controllers/actions.rs
+++ src/controllers/actions.rs
@@ -65,8 +65,10 @@ impl HttpActions {
     }
 
     pub fn register(&mut self, action: HttpAction) -> Result<(), String> {
-        for action in &self.actions {
-            if action.http_route.route.to_lowercase() == action.http_route.route.to_lowercase() {
+        for registered_action in &self.actions {
+            if registered_action.http_route.route.to_lowercase()
+                == action.http_route.route.to_lowercase()
+            {
                 return Err(format!(
                     "Route {} is already registered",
                     action.http_route.route

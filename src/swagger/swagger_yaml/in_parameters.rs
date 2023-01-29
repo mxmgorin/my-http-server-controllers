7e5132be9f5388e57f418ea5ddf8d4fe63f732f3
amigin <amigin@gmail.com>
2023-01-29 17:59:45 +0200

    Fixes

--- src/swagger/swagger_yaml/in_parameters.rs
+++ src/swagger/swagger_yaml/in_parameters.rs
@@ -48,9 +48,7 @@ pub fn build(yaml_writer: &mut YamlWriter, action_description: &HttpActionDescri
         for param in body_params {
             if let Some(param_type) = get_param_type(&param.field.data_type) {
                 yaml_writer.write("name", param.field.name.as_str());
-                yaml_writer.increase_level();
                 yaml_writer.write("type", param_type);
-                yaml_writer.decrease_level();
             }
         }
         yaml_writer.decrease_level();

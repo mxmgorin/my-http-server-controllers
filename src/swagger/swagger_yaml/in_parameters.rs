39267cbb3184a82a6c3d6ed413c80a97357aa8a6
amigin <amigin@gmail.com>
2023-01-29 19:44:51 +0200

    Fixes

--- src/swagger/swagger_yaml/in_parameters.rs
+++ src/swagger/swagger_yaml/in_parameters.rs
@@ -105,11 +105,19 @@ fn write_body_input_param(yaml_writer: &mut YamlWriter, field: &HttpField) {
             yaml_writer.write_empty(field.name.as_str());
             yaml_writer.increase_level();
 
-            yaml_writer.write(
-                "$ref",
-                format!("#/components/schemas/{}", object.struct_id).as_str(),
-            );
+            yaml_writer.write("type", "object");
+            yaml_writer.write_empty("properties");
+            yaml_writer.increase_level();
+            for obj_field in &object.fields {
+                write_body_input_param(yaml_writer, obj_field);
+            }
 
+            //            yaml_writer.write(
+            //               "$ref",
+            //              format!("#/components/schemas/{}", object.struct_id).as_str(),
+            //          );
+
+            yaml_writer.decrease_level();
             yaml_writer.decrease_level();
             yaml_writer.decrease_level();
         }

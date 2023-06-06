3be9fa0c15ed5b2eb5decaa7dd387a1ba584e2e4
amigin <amigin@gmail.com>
2023-06-06 23:40:55 +0300

    Debugging

--- src/controllers/documentation/data_types/data_type_provider.rs
+++ src/controllers/documentation/data_types/data_type_provider.rs
@@ -112,6 +112,8 @@ impl<T: DataTypeProvider> DataTypeProvider for Vec<T> {
         match data_type {
             HttpDataType::SimpleType(tp) => HttpDataType::ArrayOf(ArrayElement::SimpleType(tp)),
             HttpDataType::Object(obj) => HttpDataType::ArrayOf(ArrayElement::Object(obj)),
+            HttpDataType::ArrayOf(item) => HttpDataType::ArrayOf(item),
+            HttpDataType::Enum(item) => HttpDataType::Enum(item),
             _ => panic!("Unsupported data type: {:?}", data_type),
         }
     }

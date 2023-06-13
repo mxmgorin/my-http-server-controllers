d018f3e96556679d7e80f323b45adc28100cfa5c
amigin <amigin@gmail.com>
2023-06-13 16:36:24 +0300

    Added Generic Type when we have get_data_type()

--- src/controllers/documentation/data_types/data_type_provider.rs
+++ src/controllers/documentation/data_types/data_type_provider.rs
@@ -7,108 +7,108 @@ use serde::de::DeserializeOwned;
 use super::{ArrayElement, HttpDataType, HttpSimpleType};
 
 pub trait DataTypeProvider {
-    fn get_data_type() -> HttpDataType;
+    fn get_data_type(generic_type: Option<&'static str>) -> HttpDataType;
 }
 
 impl DataTypeProvider for u8 {
-    fn get_data_type() -> HttpDataType {
+    fn get_data_type(_generic_type: Option<&'static str>) -> HttpDataType {
         HttpDataType::SimpleType(HttpSimpleType::Byte)
     }
 }
 
 impl DataTypeProvider for i8 {
-    fn get_data_type() -> HttpDataType {
+    fn get_data_type(_generic_type: Option<&'static str>) -> HttpDataType {
         HttpDataType::SimpleType(HttpSimpleType::Byte)
     }
 }
 
 impl DataTypeProvider for u16 {
-    fn get_data_type() -> HttpDataType {
+    fn get_data_type(_generic_type: Option<&'static str>) -> HttpDataType {
         HttpDataType::SimpleType(HttpSimpleType::Integer)
     }
 }
 
 impl DataTypeProvider for i16 {
-    fn get_data_type() -> HttpDataType {
+    fn get_data_type(_generic_type: Option<&'static str>) -> HttpDataType {
         HttpDataType::SimpleType(HttpSimpleType::Integer)
     }
 }
 
 impl DataTypeProvider for u32 {
-    fn get_data_type() -> HttpDataType {
+    fn get_data_type(_generic_type: Option<&'static str>) -> HttpDataType {
         HttpDataType::SimpleType(HttpSimpleType::Integer)
     }
 }
 
 impl DataTypeProvider for i32 {
-    fn get_data_type() -> HttpDataType {
+    fn get_data_type(_generic_type: Option<&'static str>) -> HttpDataType {
         HttpDataType::SimpleType(HttpSimpleType::Integer)
     }
 }
 
 impl DataTypeProvider for u64 {
-    fn get_data_type() -> HttpDataType {
+    fn get_data_type(_generic_type: Option<&'static str>) -> HttpDataType {
         HttpDataType::SimpleType(HttpSimpleType::Long)
     }
 }
 
 impl DataTypeProvider for i64 {
-    fn get_data_type() -> HttpDataType {
+    fn get_data_type(_generic_type: Option<&'static str>) -> HttpDataType {
         HttpDataType::SimpleType(HttpSimpleType::Long)
     }
 }
 
 impl DataTypeProvider for usize {
-    fn get_data_type() -> HttpDataType {
+    fn get_data_type(_generic_type: Option<&'static str>) -> HttpDataType {
         HttpDataType::SimpleType(HttpSimpleType::Long)
     }
 }
 
 impl DataTypeProvider for isize {
-    fn get_data_type() -> HttpDataType {
+    fn get_data_type(_generic_type: Option<&'static str>) -> HttpDataType {
         HttpDataType::SimpleType(HttpSimpleType::Long)
     }
 }
 
 impl DataTypeProvider for f32 {
-    fn get_data_type() -> HttpDataType {
+    fn get_data_type(_generic_type: Option<&'static str>) -> HttpDataType {
         HttpDataType::SimpleType(HttpSimpleType::Double)
     }
 }
 
 impl DataTypeProvider for f64 {
-    fn get_data_type() -> HttpDataType {
+    fn get_data_type(_generic_type: Option<&'static str>) -> HttpDataType {
         HttpDataType::SimpleType(HttpSimpleType::Double)
     }
 }
 
 impl DataTypeProvider for bool {
-    fn get_data_type() -> HttpDataType {
+    fn get_data_type(_generic_type: Option<&'static str>) -> HttpDataType {
         HttpDataType::SimpleType(HttpSimpleType::Boolean)
     }
 }
 
 impl DataTypeProvider for String {
-    fn get_data_type() -> HttpDataType {
+    fn get_data_type(_generic_type: Option<&'static str>) -> HttpDataType {
         HttpDataType::SimpleType(HttpSimpleType::String)
     }
 }
 
 impl<'s> DataTypeProvider for &'s str {
-    fn get_data_type() -> HttpDataType {
+    fn get_data_type(_generic_type: Option<&'static str>) -> HttpDataType {
         HttpDataType::SimpleType(HttpSimpleType::String)
     }
 }
 
 impl<'s> DataTypeProvider for DateTimeAsMicroseconds {
-    fn get_data_type() -> HttpDataType {
+    fn get_data_type(_generic_type: Option<&'static str>) -> HttpDataType {
         HttpDataType::SimpleType(HttpSimpleType::DateTime)
     }
 }
 
 impl<T: DataTypeProvider> DataTypeProvider for Vec<T> {
-    fn get_data_type() -> HttpDataType {
-        let data_type = T::get_data_type();
+    fn get_data_type(generic_type: Option<&'static str>) -> HttpDataType {
+        let data_type = T::get_data_type(generic_type);
         match data_type {
             HttpDataType::SimpleType(tp) => HttpDataType::ArrayOf(ArrayElement::SimpleType(tp)),
             HttpDataType::Object(obj) => HttpDataType::ArrayOf(ArrayElement::Object(obj)),
@@ -119,32 +119,35 @@ impl<T: DataTypeProvider> DataTypeProvider for Vec<T> {
 }
 
 impl<TValue: DataTypeProvider> DataTypeProvider for HashMap<String, TValue> {
-    fn get_data_type() -> HttpDataType {
-        match TValue::get_data_type() {
+    fn get_data_type(generic_type: Option<&'static str>) -> HttpDataType {
+        match TValue::get_data_type(generic_type) {
             HttpDataType::SimpleType(tp) => {
                 HttpDataType::DictionaryOf(ArrayElement::SimpleType(tp))
             }
             HttpDataType::Object(obj) => HttpDataType::DictionaryOf(ArrayElement::Object(obj)),
             HttpDataType::ArrayOf(item) => HttpDataType::DictionaryOfArray(item),
-            _ => panic!("Unsupported data type {:?}", TValue::get_data_type()),
+            _ => panic!(
+                "Unsupported data type {:?}",
+                TValue::get_data_type(generic_type)
+            ),
         }
     }
 }
 
 impl DataTypeProvider for FileContent {
-    fn get_data_type() -> HttpDataType {
+    fn get_data_type(_generic_type: Option<&'static str>) -> HttpDataType {
         HttpDataType::SimpleType(HttpSimpleType::Binary)
     }
 }
 
 impl DataTypeProvider for RawData {
-    fn get_data_type() -> HttpDataType {
+    fn get_data_type(_generic_type: Option<&'static str>) -> HttpDataType {
         HttpDataType::SimpleType(HttpSimpleType::Binary)
     }
 }
 
 impl<T: DeserializeOwned + DataTypeProvider> DataTypeProvider for RawDataTyped<T> {
-    fn get_data_type() -> HttpDataType {
-        T::get_data_type()
+    fn get_data_type(generic_type: Option<&'static str>) -> HttpDataType {
+        T::get_data_type(generic_type)
     }
 }

a059c076aedcc3b03a6093853932edff9aebdce6
amigin <amigin@gmail.com>
2023-07-19 01:01:51 +0300

    Removed Default Value from HttpField

--- src/controllers/documentation/data_types/http_field.rs
+++ src/controllers/documentation/data_types/http_field.rs
@@ -7,21 +7,14 @@ pub struct HttpField {
     pub name: String,
     pub data_type: HttpDataType,
     pub required: bool,
-    pub default_value: Option<&'static str>,
 }
 
 impl HttpField {
-    pub fn new(
-        name: &str,
-        data_type: HttpDataType,
-        required: bool,
-        default_value: Option<&'static str>,
-    ) -> Self {
+    pub fn new(name: &str, data_type: HttpDataType, required: bool) -> Self {
         Self {
             name: name.to_string(),
             data_type,
             required,
-            default_value,
         }
     }
 
